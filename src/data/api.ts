8db388ad16d8456943ad1c047088e9cfde1560fe
Guilherme Andrade <gui-keanne@hotmail.com>
2024-02-18 19:40:29 -0300

    fetching dos produtos na home

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,7 +1,11 @@
 import { env } from "@/env";
 
-export async function api(path: string, init?: RequestInit) {
+export async function api(
+  path: string,
+  init?: RequestInit,
+  apiPrefix: string = "/api",
+) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL;
-  const url = new URL(path, baseURL);
+  const url = new URL(apiPrefix.concat(path), baseURL);
   return await fetch(url, init);
 }

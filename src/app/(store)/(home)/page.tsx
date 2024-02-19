dd2d4a21fcd0da3f80fc35877ef13c988c1e65d1
Guilherme Andrade <gui-keanne@hotmail.com>
2024-02-19 10:27:03 -0300

    carregando produto especifico dos dados

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,20 +1,20 @@
-import { api } from "@/data/api";
-import { Product } from "@/data/types/product";
-import Image from "next/image";
-import Link from "next/link";
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import Image from 'next/image'
+import Link from 'next/link'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured", {
+  const response = await api('/products/featured', {
     next: {
       revalidate: 60 * 60, // 1 hora
     },
-  });
-  const featuredProducts = await response.json();
-  return featuredProducts;
+  })
+  const featuredProducts = await response.json()
+  return featuredProducts
 }
 
 export default async function Home() {
-  const [highLightedProduct, ...otherProducts] = await getFeaturedProducts();
+  const [highLightedProduct, ...otherProducts] = await getFeaturedProducts()
 
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
@@ -34,9 +34,9 @@ export default async function Home() {
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="text-sm truncate">{highLightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            {highLightedProduct.price.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
+            {highLightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
               minimumFractionDigits: 0,
               maximumFractionDigits: 0,
             })}
@@ -63,17 +63,17 @@ export default async function Home() {
             <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
               <span className="text-sm truncate">{product.title}</span>
               <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                {product.price.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
                   minimumFractionDigits: 0,
                   maximumFractionDigits: 0,
                 })}
               </span>
             </div>
           </Link>
-        );
+        )
       })}
     </div>
-  );
+  )
 }

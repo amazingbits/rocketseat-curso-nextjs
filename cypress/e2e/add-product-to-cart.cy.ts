65a9b6724eeeb83d16f0271c67a4b03d2efc1c3d
Guilherme Andrade <gui-keanne@hotmail.com>
2024-02-20 10:40:22 -0300

    criando comandos e testes de busca no cypress

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,6 +1,9 @@
 describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   it('should be possible navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
     cy.get('a[href^="/product"]').first().click()
     cy.url().should('include', '/product')
     cy.contains('adicionar ao carrinho', { matchCase: false }).click()
@@ -8,7 +11,6 @@ describe('add product to cart', () => {
   })
 
   it('should not duplicate same product in cart', () => {
-    cy.visit('http://localhost:3000')
     cy.get('a[href^="/product"]').first().click()
     cy.url().should('include', '/product')
     cy.contains('adicionar ao carrinho', { matchCase: false }).click()
@@ -17,8 +19,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to cart', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('input[name=q]').type('camiseta').parent('form').submit()
+    cy.searchByQuery('camiseta')
     cy.get('a[href^="/product"]').first().click()
     cy.contains('adicionar ao carrinho', { matchCase: false }).click()
     cy.contains('cart (1)', { matchCase: false }).should('exist')
